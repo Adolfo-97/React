e8a1833afaf691fb3b55180982f77ac060935193
Adolfo-97 <adolfogomez@live.it>
2023-11-05 16:19:46 +0100

    Upload esercizio

--- exercise/src/Index.jsx
+++ exercise/src/Index.jsx
@@ -1,4 +1,4 @@
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { Hello } from "./Hello";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<Hello />);
