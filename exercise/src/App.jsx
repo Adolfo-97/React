7187bf1e3524ce27dd5fd15399c5093ad122d856
Adolfo-97 <adolfogomez@live.it>
2023-11-05 16:33:33 +0100

    Upload esercizio, commento dentro App.jsx

--- exercise/src/App.jsx
+++ exercise/src/App.jsx
@@ -1,9 +1,12 @@
 import { Hello } from "./Hello";
-
+import { Message } from "./Message";
 export function App() {
   return (
     <div>
       <Hello />
+      <Hello />
+      <Message />
     </div>
   );
 }
+/* Yes it can be used more than once and I can also render the Message component on the App componenet as long I use the correct Import function*/
