8108e1d8989e92eb818a4ecb814f3342385bc426
Adolfo-97 <adolfogomez@live.it>
2023-11-05 19:30:13 +0100

    quicksave

--- exercise/src/App.jsx
+++ exercise/src/App.jsx
@@ -1,12 +1,10 @@
-import { Hello } from "./Hello";
-import { Message } from "./Message";
+import { Welcome } from "./Welcome";
+
 export function App() {
   return (
     <div>
-      <Hello />
-      <Hello />
-      <Message />
+      <Welcome />
+      {Mario}
     </div>
   );
 }
-/* Yes it can be used more than once and I can also render the Message component on the App componenet as long I use the correct Import function*/
