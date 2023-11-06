9209c92fa5aedc099104bf99d240da5df0b757f0
Adolfo-97 <adolfogomez@live.it>
2023-11-06 11:56:36 +0100

    upload esercizio

--- exercise/src/MouseClicker.jsx
+++ exercise/src/MouseClicker.jsx
@@ -1,9 +1,12 @@
 export function MouseClicker() {
   function handlebuttonClick(event) {
-    console.log(event.target.name);
+    console.log(
+      event.target.src
+    ); /* We prevent to name attribute of the button from bein printed by pointing the src content from the event instead of the name */
   }
   return (
     <button name="one" onClick={handlebuttonClick}>
+      <img src="Ciao" width={24} height={24} />
       Click me
     </button>
   );
