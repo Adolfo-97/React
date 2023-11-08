9fef4c4336e9ce5482c8338709ac1c718157f2d9
Adolfo-97 <adolfogomez@live.it>
2023-11-08 10:58:23 +0100

    upload esercizio

---
+++ exercise/src/Login.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Welcome from "./Welcome";
+
+function Login() {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    checkbox: "",
+  });
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    const checked = event.target.checked;
+    const type = event.target.type;
+    setData((d) => {
+      const updated = {
+        ...d,
+        [name]: type === "checkbox" ? checked : value,
+      };
+
+      return updated;
+    });
+  }
+
+  /*   console.log(event.target);
+   */
+  return (
+    <div>
+      <Welcome name={data.username} />
+      <input name="username" value={data.username} onChange={handleChange} />
+      <input
+        name="password"
+        type="password"
+        value={data.password}
+        onChange={handleChange}
+      />
+      <input
+        name="checkbox"
+        type="checkbox"
+        checked={data.checkbox}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+export default Login;
