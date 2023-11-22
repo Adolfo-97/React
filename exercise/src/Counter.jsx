240b84645453f4456f8b85b5313133226630cb1b
Adolfo-97 <adolfogomez@live.it>
2023-11-22 20:51:46 +0100

    fix

--- exercise/src/Counter.jsx
+++ exercise/src/Counter.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 
-export function Counter() {
-  const [counter, setCounter] = useState(0);
+export function Counter({ initialValue = 99, increment = 1 }) {
+  const [counter, setCounter] = useState(initialValue);
 
-  function handleButtonIncrement() {
-    setCounter(counter + 1);
+  function handleIncrement() {
+    setCounter((counter) => counter + increment);
+    //It's important to use a function to deal with asynchronous updates, like in this case we want to update the latest "version" of our counter, an immedediate
+    //value may update the value only once at the time even if we do more increments inside a function.
   }
-
   return (
     <div>
-      <h2>Counter to {counter}</h2>
-      <button onClick={handleButtonIncrement}>Increment value</button>
+      <h2>The count is at : {counter}</h2>
+      <button onClick={handleIncrement}>Increment</button>
     </div>
   );
 }
