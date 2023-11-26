721286ee110c1bf1bd7fca5429980ecc92b0ed35
Adolfo-97 <adolfogomez@live.it>
2023-11-26 11:55:05 +0100

    upload esercizio

--- exercise/src/App.jsx
+++ exercise/src/App.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
+import { Counter } from "./Counter";
 
 export function App({ name }) {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Welcome name="Mark" />} />
+        <Route
+          path="/Counter"
+          element={<Counter initialValue={0} increment={1} />}
+        />
       </Routes>
     </div>
   );
