9b597ad8a124f09cc3b2a8efbe0836a11f9357b1
Adolfo-97 <adolfogomez@live.it>
2023-11-26 17:03:57 +0100

    upload esercizio

--- exercise/src/App.jsx
+++ exercise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
@@ -6,8 +6,15 @@ import { ShowGithubUser } from "./ShowGithubUser";
 export function App({ name }) {
   return (
     <div>
+      <Link to="/">Home</Link>
+      <hr />
+      <Link to="/counter">Counter</Link>
+      <hr />
+      <Link to="/users/Adolfo-97">Github User!</Link>
       <Routes>
-        <Route path="users/:username" element={<ShowGithubUser />} />
+        <Route path="/" element={<Welcome name="Mark" />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
       </Routes>
     </div>
   );
